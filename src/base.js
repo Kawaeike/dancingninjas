0920998a0468b04d5190f2f94bb4a01981dd3776
Eike Kawan <e.kawan@mso-digital.de>
2017-10-25 16:34:32 +0200

    firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyDYDS15F0s7rxBAsCaNk_Cz_UmTpLF-Nzo",
+  authDomain: "dancingninjas-884eb.firebaseapp.com",
+  databaseURL: "https://dancingninjas-884eb.firebaseio.com",
+});
+
+export default base;
